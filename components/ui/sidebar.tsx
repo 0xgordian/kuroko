36459b6ab5d7dcedf409a31e38f14fdaacfd8842
Gordian Etim <etimgordian@gmail.com>
2026-05-01 04:33:15 +0100

    [098] ui/sidebar: replace PanelLeft with Solar sidebar-minimalistic-linear

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-import { PanelLeft } from "lucide-react"
+import { Icon } from "@iconify/react"
 
 import { useIsMobile } from "@/hooks/use-mobile"
 import { cn } from "@/lib/utils"
@@ -287,7 +287,7 @@ const SidebarTrigger = React.forwardRef<
       }}
       {...props}
     >
-      <PanelLeft />
+      <Icon icon="solar:sidebar-minimalistic-linear" />
       <span className="sr-only">Toggle Sidebar</span>
     </Button>
   )
